adaa9f9a88b6556bc052b57c738982284b1bab0f
Victor Gabriel Valencia García <vgabriel.valencia@gmail.com>
2023-02-24 03:23:42 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'angularJs_CaridApp_Integradora',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
